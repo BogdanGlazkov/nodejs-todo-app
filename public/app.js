85a70adf285b395ece58d0705d94e9ee18f7d911
BogdanGlazkov <bglazkov@i.ua>
2023-04-07 23:38:43 +0300

    Add todo model

--- public/app.js
+++ public/app.js
@@ -8,19 +8,33 @@ new Vue({
       todos: [],
     };
   },
+  created() {
+    fetch("/api/todo", {
+      method: "get",
+    })
+      .then((res) => res.json())
+      .then((todos) => {
+        this.todos = todos;
+      })
+      .catch((err) => console.log(err));
+  },
   methods: {
     addTodo() {
       const title = this.todoTitle.trim();
       if (!title) {
         return;
       }
-      this.todos.push({
-        title: title,
-        id: Math.random(),
-        done: false,
-        date: new Date(),
-      });
-      this.todoTitle = "";
+      fetch("/api/todo", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title }),
+      })
+        .then((res) => res.json())
+        .then(({ todo }) => {
+          this.todos.push(todo);
+          this.todoTitle = "";
+        })
+        .catch((err) => console.log(err));
     },
     removeTodo(id) {
       this.todos = this.todos.filter((t) => t.id !== id);
@@ -30,12 +44,18 @@ new Vue({
     capitalize(value) {
       return value.toString().charAt(0).toUpperCase() + value.slice(1);
     },
-    date(value) {
-      return new Intl.DateTimeFormat("ru-RU", {
+    date(value, withTime) {
+      const options = {
         year: "numeric",
         month: "long",
         day: "2-digit",
-      }).format(new Date(value));
+      };
+      if (withTime) {
+        options.hour = "2-digit";
+        options.minute = "2-digit";
+        options.second = "2-digit";
+      }
+      return new Intl.DateTimeFormat("en-US", options).format(new Date(value));
     },
   },
 });
