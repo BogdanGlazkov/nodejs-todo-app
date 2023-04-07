13f9d377a12ea84d3d95283838aebd3bef89177f
BogdanGlazkov <bglazkov@i.ua>
2023-04-07 23:39:05 +0300

    Add todo routes

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
+const sequelize = require("./utils/database");
+const todoRoutes = require("./routes/todo");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use("/api/todo", todoRoutes);
 
 app.use((req, res, next) => {
   res.sendFile("/index.html");
 });
 
-app.listen(PORT);
+async function start() {
+  try {
+    await sequelize.sync();
+    app.listen(PORT);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
